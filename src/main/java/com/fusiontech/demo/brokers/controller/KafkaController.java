6774f5faf1c128909102e6857fdb364f3e2b18b7
fusion <fusion@MacBook-Pro-Fusion-4.local>
2024-12-12 14:03:25 +0300

    changed api

--- src/main/java/com/fusiontech/demo/brokers/controller/KafkaController.java
+++ src/main/java/com/fusiontech/demo/brokers/controller/KafkaController.java
@@ -1,15 +1,15 @@
 package com.fusiontech.demo.brokers.controller;
 
 import com.fusiontech.demo.brokers.constant.KafkaTopic;
-import com.fusiontech.demo.brokers.dto.KafkaRequest;
+import com.fusiontech.demo.brokers.dto.RabbitRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,19 +20,11 @@ public class KafkaController {
 
   private final KafkaTemplate<String, Object> kafkaTemplate;
 
-  @PostMapping("/one-message")
-  public ResponseEntity<String> sendMessage(@RequestBody KafkaRequest request) {
-	kafkaTemplate.send(KafkaTopic.KAFKA_TOPIC, request);
-	return ResponseEntity.ok("Сообщение отправлено в Kafka");
-  }
-
-  @PostMapping("/messages")
-  public ResponseEntity<String> sendMessage(@RequestBody KafkaRequest request,
-											@RequestParam int count) {
+  @PostMapping("/messages/{count}")
+  public ResponseEntity<String> sendMessage(@RequestBody RabbitRequest request, @PathVariable int count) {
 	for (int i = 0; i < count; i++) {
 	  kafkaTemplate.send(KafkaTopic.KAFKA_TOPIC, request);
-	  log.info("сообщение отправлено");
 	}
-	return ResponseEntity.ok("Сообщения отправлены в Kafka");
+	return ResponseEntity.ok("Сообщение отправлено в RabbitMQ");
   }
 }
--- src/main/java/com/fusiontech/demo/brokers/controller/RabbitMqController.java
+++ src/main/java/com/fusiontech/demo/brokers/controller/RabbitMqController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,17 +22,11 @@ public class RabbitMqController {
 
   private final RabbitTemplate rabbitTemplate;
 
-  @PostMapping("/one-message")
-  public ResponseEntity<String> sendMessage(@RequestBody RabbitRequest request) {
-	rabbitTemplate.convertAndSend(RabbitExchange.TEST_EXCHANGE, RabbitRoutingKey.TEST_ROUTING_KEY, request);
-	return ResponseEntity.ok("Сообщение отправлено в RabbitMQ");
-  }
-
-  @PostMapping("/messages")
-  public ResponseEntity<String> sendMessages(@RequestBody RabbitRequest request, @RequestParam int count) {
+  @PostMapping("/messages/{count}")
+  public ResponseEntity<String> sendMessage(@RequestBody RabbitRequest request, @PathVariable int count) {
 	for (int i = 0; i < count; i++) {
 	  rabbitTemplate.convertAndSend(RabbitExchange.TEST_EXCHANGE, RabbitRoutingKey.TEST_ROUTING_KEY, request);
 	}
-	return ResponseEntity.ok("Сообщения отправлены в RabbitMQ");
+	return ResponseEntity.ok("Сообщение отправлено в RabbitMQ");
   }
 }
